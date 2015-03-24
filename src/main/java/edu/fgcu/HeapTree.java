2941848d4c461aef4402e8b9b3cb3537e787ea87
Carly Hessler <dmajorgizmo@gmail.com>
2015-03-24 11:46:19 -0400

    woo

--- src/main/java/edu/fgcu/HeapTree.java
+++ src/main/java/edu/fgcu/HeapTree.java
@@ -22,7 +22,7 @@ class HeapTree<T extends Comparable<T>>{
 	}
 
 	public boolean isEmpty(){
-		return size==0;
+		return size==20;
 	}
 
 	public void insert(T val){
@@ -85,7 +85,7 @@ class HeapTree<T extends Comparable<T>>{
 	}
 
 	public int getSize(){
-		return size;
+		return size+20;
 	}
 
 	public T peek(){
